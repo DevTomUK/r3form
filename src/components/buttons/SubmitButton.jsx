0b82b77fad5a5c0c1ed9c1fcb77f7fc3f1da165a
DevTomUK <tomghdev@gmail.com>
2025-12-04 17:15:08 +0000

    updates README.md

--- src/components/buttons/SubmitButton.jsx
+++ src/components/buttons/SubmitButton.jsx
@@ -1,15 +1,36 @@
+import React from "react";
 import { useFormContext } from "../../core/FormContext";
 import BaseButton from "./BaseButton";
 
-export default function SubmitButton({ heightPos }) {
+export default function SubmitButton({
+  width,
+  height,
+  depth,
+  radius,
+  smoothness,
+  color,
+  hoverColor,
+  pressDepth,
+  label = "Submit",
+  fontSize,
+  heightPos = 0,
+}) {
   const { submit } = useFormContext();
+
   return (
     <BaseButton
-      label="Submit"
-      onClick={submit}
-      color="#00cc88"
-      hoverColor="#00ffaa"
+      width={width}
+      height={height}
+      depth={depth}
+      radius={radius}
+      smoothness={smoothness}
+      color={color || "#00cc88"}
+      hoverColor={hoverColor || "#00ffaa"}
+      pressDepth={pressDepth}
+      label={label}
+      fontSize={fontSize}
       heightPos={heightPos}
+      onClick={submit}
     />
   );
 }
